b315dfef57d15d1318621c15294585218b17d6b1
riefkyarif <riefkyar.ed@protonmail.com>
2022-05-09 09:28:18 +0700

    ccorrection

--- Learning/constructor.py
+++ Learning/constructor.py
@@ -6,9 +6,9 @@ class Hero:
         self.power = inputPower
         self.armor = inputArmor
 
-hero1 = Hero{"sniper", 100, 10, 4}
-hero2 = Hero{"Franco", 1000, 5, 10}
-hero3 = Hero{"Atlas", 500, 15, 9}
+hero1 = Hero("sniper", 100, 10, 4)
+hero2 = Hero("Franco", 1000, 5, 10)
+hero3 = Hero("Atlas", 500, 15, 9)
 
 print(hero1.__dict__)
 print(hero2.__dict__)
